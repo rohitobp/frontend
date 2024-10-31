348399669bcab5df600eecb113ee41816971ff49
ROHIT SANDHU <rohit.obp@gmail.com>
2024-10-31 08:36:02 +0100

    .

--- src/app/locale/page.tsx
+++ src/app/locale/page.tsx
@@ -1,7 +1,12 @@
-import Image from "next/image";
-
-export default function Home() {
+import {useTranslations} from 'next-intl';
+import {Link} from '@/i18n/routing';
+ 
+export default function HomePage() {
+  const t = useTranslations('HomePage');
   return (
-    <h1>Hello!</h1>
+    <div>
+      <h1>{t('title')}</h1>
+      <Link href="/about">{t('about')}</Link>
+    </div>
   );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,9 @@
-import createMiddleware from "next-intl/middleware";
-
-
-export default createMiddleware({
-    locales: ['en','es', 'es'],
-    defaultLocale: 'en',
-});
-
+import createMiddleware from 'next-intl/middleware';
+import {routing} from './i18n/routing';
+ 
+export default createMiddleware(routing);
+ 
 export const config = {
-    matcher: ['/', '/(en|es|ca)/:path*']
-}
+  // Match only internationalized pathnames
+  matcher: ['/', '/(de|en)/:path*']
+};
