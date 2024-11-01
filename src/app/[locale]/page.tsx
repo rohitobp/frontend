3a3fa44b1062b30c95981e318ad17f67ae2e63b1
ROHIT SANDHU <rohit.obp@gmail.com>
2024-11-01 10:51:11 +0100

    .

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import {useTranslations} from 'next-intl';
+
+export default function Home() {
+
+  const t = useTranslations("IndexPage");
+  return (
+    <p className="max-w-[590px]">
+      
+
+    </p>
+  );
+}
